c3adde4dd3ccdda97ddb2ba0a7b0ffb0246cb0f4
Marc Höppner <marc.hoeppner@mailbox.org>
2023-10-21 00:33:00 +0200

    Clean up infrastructure

--- backend/create_server.sh
+++ backend/create_server.sh
@@ -1,5 +1,9 @@
 TOKEN=$(cat ../.linode_token)
 
+# Create key
+ssh-keygen -o -a 100 -t ed25519 -f .flotteLotte -N ""
+PUBKEY=$(cat .flotteLotte.pub)
+
 curl -X POST https://api.linode.com/v4/linode/instances \
     -H "Authorization: Bearer $TOKEN" -H "Content-type: application/json" \
-    -d '{"type": "g6-nanode-1", "region": "eu-central", "image": "linode/debian11", "root_pass": "x{c#[o(!Hf#JyB9hNsZ&e!bJ(+[]9E", "label": "flotteLotte_backend"}'
+    -d '{"type": "g6-nanode-1", "region": "eu-central", "image": "linode/debian11", "root_pass": "xc#[o(!Hf#JyB9hNsZ&e!bJ(+[]9E", "label": "flotteLotte_backend", "authorized_keys": [ "'"$PUBKEY"'" ]}'
--- backend/destroy_server.sh
+++ backend/destroy_server.sh
@@ -7,5 +7,5 @@ LINODES=$(curl -H "Authorization: Bearer $TOKEN" \
 LINODEID=$(echo $LINODES | jq '.data.[] | select(.label=="flotteLotte_backend") | .id')
 
 curl -H "Authorization: Bearer $TOKEN" \
-    https://api.linode.com/v4/linode/instances/$LINODEID
+     -X DELETE https://api.linode.com/v4/linode/instances/$LINODEID
 
