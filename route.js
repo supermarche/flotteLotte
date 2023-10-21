b60710c8c1389d75c92df5b7f2d4e4a06def4be1
myslide <jess@schied.info>
2023-10-21 09:59:33 +0200

    Markersdorf in the center

--- route.js
+++ route.js
@@ -1,10 +1,9 @@
-const L = require("leaflet");
-var map = L.map('map').setView([51.505, -0.09], 13);
+var map = L.map('map').setView([51.146, 14.8789], 13);
 
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
 
-L.marker([51.5, -0.09]).addTo(map)
+L.marker([51.146, 14.8789]).addTo(map)
     .bindPopup('A pretty CSS popup.<br> Easily customizable.')
     .openPopup();
