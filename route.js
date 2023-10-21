19ff43a7782e583601d4b30c252a74758b1637d2
myslide <jess@schied.info>
2023-10-21 15:22:09 +0200

    Matching.html Zeigt routing

--- route.js
+++ route.js
@@ -7,3 +7,21 @@ L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
 L.marker([51.146, 14.8789]).addTo(map)
     .bindPopup('A pretty CSS popup.<br> Easily customizable.')
     .openPopup();
+
+L.Control.geocoder().addTo(map);
+
+function onMapClick(e) {
+        alert("You clicked the map at " + e.latlng);
+    }
+    
+    map.on('click', onMapClick);
+
+    function onSetPickUp(e) {
+        alert("You set the map at " + e.latlng);
+    }
+
+    function onSetPickUp() {
+        alert("You set the map at" );
+    }
+    
+    
